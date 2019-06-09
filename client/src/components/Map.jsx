f73cc11132ee4f606f2ad99242bf3966675fe631
Harrison Wu <harrisonywu@gmail.com>
2019-06-09 15:24:05 -0700

    Refactor Map component and add comments detailing Search functionality

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 import { TOKEN_GOOGLE_MAPS } from '../config/mapsConfig.js';
 import GoogleMapReact from 'google-map-react';
 import parkData from '../data/parkData.js'
-import styles from './Map.css'
 
 
-// Creates markers for each 
-const apiIsLoaded = (map, maps, parkData) => {
+// Creates markers for each park in this.state.parkData
+const loadMarkers = (map, maps, parkData) => {
   let markers = [];
   
   parkData.forEach((park, index) => {
@@ -24,6 +23,7 @@ const apiIsLoaded = (map, maps, parkData) => {
     markers.push(marker);
   })
 
+  // creates marker clusters for all parks on the map
   let markerCluster = new MarkerClusterer(map, markers,
     {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 };
@@ -32,27 +32,39 @@ class Map extends Component {
     super();
 
     this.state = {
-      parkData
+      parkData,
+      center: {
+        lat: 31.9686,
+        lng: -99.9018
+      },
+      zoom: 6
     };
   }
-  static defaultProps = {
-    center: {
-      lat: 31.9686,
-      lng: -99.9018
-    },
-    zoom: 6
 
-  };
+  /* onChange for Search
+    updates a search term in state.
+  */
+
+  /* Search Function
+   Should modify parkData state
+   Calls loadMarkers passing in the now updated state.
+   This should result in a new markers/new marker cluster
+   DON'T FORGET TO ALLOW FOR ALL THE PARKS TO RERENDER IF YOU PRESS RESET/SEARCH FOR EMPTY TERM
+  */
+
+
+
 
   render() {
+    const { center, zoom, parkData } = this.state;
     return (
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: TOKEN_GOOGLE_MAPS }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
+          defaultCenter={center}
+          defaultZoom={zoom}
           yesIWantToUseGoogleMapApiInternals
-          onGoogleApiLoaded={({ map, maps }) => apiIsLoaded(map, maps, this.state.parkData)}
+          onGoogleApiLoaded={({ map, maps }) => loadMarkers(map, maps, parkData)}
         ></GoogleMapReact>
       </div>
     );
